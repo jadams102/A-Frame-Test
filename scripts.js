aabe061f091b63d7de71a883799f7d711faf8f0d
Joel Adams <joeladamsdesign@gmail.com>
2018-07-12 22:25:09 -0700

    add back and next image buttons and related functions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
-let images = ["#coast","#garden","#falcon", "#ocean", "#paris", "#night"]
-let currentImgIndex = 1;
+$(document).ready(function() {
+  let images = ["#coast", "#garden", "#falcon", "#ocean", "#paris", "#night"];
+  let currentImgIndex = 0;
 
-function NextImage(){
-    $("#360Image").attr("src",images[currentImgIndex + 1])
+  function NextImage() {
+      if (currentImgIndex < images.length - 1) {
+        document.getElementById("360image").setAttribute("src", images[currentImgIndex + 1])
+        currentImgIndex ++; 
+      }
+  }
+
+  function PreviousImage() {
+    if (currentImgIndex > 0) {
+      document.getElementById("360image").setAttribute("src", images[currentImgIndex - 1])
+      currentImgIndex --; 
+    }
 }
 
-$("#next").
+  document.getElementById("next").addEventListener("click", function(evt) {
+    NextImage();
+  });
+
+  document.getElementById("previous").addEventListener("click", function(evt) {
+    PreviousImage();
+  });
+});
